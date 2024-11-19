0968bcc9f44e5903e062f769dfb8055b3dd26dbb
לב לאונטייב <152332597+ZLA964@users.noreply.github.com>
2024-11-20 01:16:20 +0300

    before printElement)

--- src/test/java/tests/SelectorsCssTest.java
+++ src/test/java/tests/SelectorsCssTest.java
@@ -6,26 +6,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class SelectorsCss {
+public class SelectorsCssTest {
     WebDriver driver = new ChromeDriver();
 
     @Test
     void elementsHeaderIlCarro() {
         driver.navigate().to("https://ilcarro.web.app/search");
-        List<WebElement> headerElements = new ArrayList<>();
+        List<WebElement> headerElements;
         System.out.println("=============== v.1 ===================");
         for (int i = 1; i <= 6; i++) {
-            String locator = ".header :nth-of-type(" + Integer.toString(i) + ")";
+            String locator = ".header :nth-of-type(" + i + ")";
             WebElement element = driver.findElement(By.cssSelector(locator));
             System.out.print(element.getTagName() + "  ");
             System.out.println(element.getText());
         }
         System.out.println("=======================================");
-      //  driver.quit();
-  //      driver.navigate().to("https://ilcarro.web.app/search");
+
         System.out.println("=============== v.2 ===================");
         headerElements = driver.findElements(By.cssSelector(".header *"));
         for (WebElement element : headerElements) {
@@ -33,13 +31,17 @@ public class SelectorsCss {
             System.out.println(element.getText());
         }
         System.out.println("=======================================");
+
         System.out.println("=============== v.3 ===================");
         WebElement element = driver.findElement(By.cssSelector(".header:nth-child(1)"));
-       System.out.print(element.getTagName() + ": ");
-        System.out.println(element.getText());
-        System.out.println("=======================================");
+        System.out.print(element.getTagName() + ": ");
+        System.out.print(element.getText());
+        System.out.println("\n  =======================================");
 
         driver.quit();
     }
 
+    private void printTagNameAndText(WebElement element) {
+
+    }
 }
