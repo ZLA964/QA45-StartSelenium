24397f359263d16e5bc8d70590d0ab42cd20e77f
לב לאונטייב <152332597+ZLA964@users.noreply.github.com>
2024-11-24 19:19:58 +0300

    one variant work some cleared

--- src/test/java/tests/ILCarroXPath.java
+++ src/test/java/tests/ILCarroXPath.java
@@ -14,10 +14,10 @@ import java.util.List;
 public class ILCarroXPath {
     WebDriver driver = new ChromeDriver();
     String[] stringsForRegistration = new String[4];
-    String name = "Firstname";
-    String lastName = "Lastname";
-    String email = "em2test@mail.org.uk";
-    String password = "Bb654321*{";
+//    String name = "Firstname";
+//    String lastName = "Lastname";
+//    String email = "em2test@mail.org.uk";
+//    String password = "Bb654321*{";
 
     @BeforeMethod
     public void setUp() {
@@ -25,10 +25,6 @@ public class ILCarroXPath {
         stringsForRegistration[1]="Lastname";
         stringsForRegistration[2]="email1@mail.com.il";
         stringsForRegistration[3]="Ab123455@!";
-        // Печать значений массива для проверки
-        for (String value : stringsForRegistration) {
-            System.out.println(value);
-            }
     }
 
     @Test
@@ -37,21 +33,22 @@ public class ILCarroXPath {
         System.out.println("========== start homework =============");
         driver.get("https://ilcarro.web.app/search");
         driver.manage().window().maximize();
-            Thread.sleep(1000);
+        Thread.sleep(1000);
         WebElement btnRegesration = driver.findElement(
-//                By.xpath("//a[text()=' Sign up ']"));  // spase before and after impotent!
             By.xpath("//a[normalize-space(text())='Sign up']")); // solution
- //           By.xpath("//a[@ng-reflect-router-link='registration']"));
+//           By.xpath("//a[@ng-reflect-router-link='registration']"));
 //            By.xpath("(//a[@class='navigation-link'])[4]"));
 //              By.xpath("(//div[@class='header']//a)[5]"));
 //              By.xpath("(//div[@class='header']//a)[last()-1]"));
+//                By.xpath("//a[text()=' Sign up ']"));  // spase before and after impotent!
 
-        System.out.println(btnRegesration.getText());
-        Thread.sleep(1000);
+//        Thread.sleep(1000);
         btnRegesration.click();
+
+// variant for next time ))
         WebElement inputName = driver.findElement(By.xpath("//input[@formcontrolname='firstName']"));
         System.out.println("Input name: " + inputName.getAttribute("ng-reflect-name"));
-
+//  search solution for click termsOfUse
 //            WebElement checkbox = driver.findElement(By.xpath("//form//input[@type='checkbox']"));
 //            System.out.println("Input name: " + checkbox.getAttribute("ng-reflect-name")+ " "
 //                    + checkbox.getAttribute("type") );
@@ -66,8 +63,8 @@ public class ILCarroXPath {
             List<WebElement> inputElements = driver.findElements(By.xpath("//form//input"));
             int count = 0;
             for (WebElement input : inputElements) {
-                System.out.println("Input name: " + input.getAttribute("ng-reflect-name")+ " "
-                        + input.getAttribute("type") );
+//                System.out.println("Input name: " + input.getAttribute("ng-reflect-name")+ " "
+//                        + input.getAttribute("type") );
                 if(count<4) {
                     System.out.println(count+ " " +  stringsForRegistration[count]);
                     input.sendKeys(stringsForRegistration[count]);
@@ -77,10 +74,7 @@ public class ILCarroXPath {
                 count++;
             }
             Thread.sleep(5000);
-
         driver.quit();
-        Thread.sleep(1000);
-        driver.get("https://ilcarro.web.app/search");
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
